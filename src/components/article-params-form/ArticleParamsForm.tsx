3700139e2cc7f6d17b67353c06600d7777f3ee88
Yulia Deltsova <juliedeltsova@yandex.ru>
2026-07-23 21:36:51 +0300

    First version

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,113 @@
+import { useState, useRef } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 import styles from './ArticleParamsForm.module.scss';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import clsx from 'clsx';
+import {
+	defaultArticleState,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
+type Props = {
+	isOpen: boolean;
+	appliedState: ArticleStateType;
+	initialState: ArticleStateType;
+	onToggle: () => void;
+	onClose: () => void;
+	onApply: (newState: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	appliedState,
+	initialState,
+	onToggle,
+	onClose,
+	onApply,
+}: Props) => {
+	const [formState, setFormState] = useState<ArticleStateType>(
+		appliedState ?? defaultArticleState
+	);
+
+	const rootRef = useRef<HTMLDivElement>(null);
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose,
+		onChange: () => {},
+	});
+
+	const handleApply = (e: React.FormEvent) => {
+		e.preventDefault();
+		onApply(formState);
+	};
+
+	const handleReset = (e?: React.FormEvent) => {
+		e?.preventDefault();
+		setFormState(initialState);
+		onApply(initialState);
+	};
+
+	const updateFormState = (field: keyof ArticleStateType, value: any) => {
+		setFormState((prev) => ({ ...prev, [field]: value }));
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={onToggle} />
+			<aside
+				ref={rootRef}
+				className={clsx(styles.container, isOpen && styles.container_open)}>
+				<form
+					className={styles.form}
+					onSubmit={handleApply}
+					onReset={handleReset}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) => updateFormState('fontFamilyOption', option)}
+					/>
+					<RadioGroup
+						name='fontSize'
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={(option) => updateFormState('fontSizeOption', option)}
+					/>
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) => updateFormState('fontColor', option)}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => updateFormState('backgroundColor', option)}
+					/>
+					<Select
+						title='Ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => updateFormState('contentWidth', option)}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
